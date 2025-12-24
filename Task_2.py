25f35cb909b7739701c69e5c6348c76bc8922ba1
Indrajeetpppp <115228004+Indrajeetpppp@users.noreply.github.com>
2025-12-25 01:32:37 +0530

    Task_2.py

---
+++ Task_2.py
@@ -0,0 +1,11 @@
+# Task 2: Creating Personalized Greeting
+
+# Step 1:
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Step 2:
+full_name = first_name + " " + last_name
+
+# Step 3:
+print("Hello, " + full_name + "! Welcome to the Python program.")
